0b3a4bcf04f41ff002362fe2f8ebda475ab0401e
yishuyan <Wang060328>
2017-05-10 12:04:44 -0400

    Add intents for the navigation part.

--- app/src/main/java/com/example/android/bustracker/MainActivity.java
+++ app/src/main/java/com/example/android/bustracker/MainActivity.java
@@ -20,7 +20,6 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -110,6 +109,7 @@ public class MainActivity extends AppCompatActivity
     private Marker marker;
     private Circle circle;
 
+    Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -132,6 +132,7 @@ public class MainActivity extends AppCompatActivity
 
         navigationView = (NavigationView) findViewById(R.id.nvView);
         navigationView.setNavigationItemSelectedListener(this);
+
 
 
         /**After press the search button, the list of buses will be displayed on the screen. */
@@ -226,25 +227,32 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = null;
-        switch(id) {
-            case R.id.nav_first_fragment:
-                fragment = new LoginActivity();
-                break;
-            case R.id.nav_second_fragment:
-                fragment = new LoginActivity();
-                break;
-            case R.id.nav_third_fragment:
-                fragment = new PreferenceActivity();
-                break;
-            default:
-                fragment = new LoginActivity();
-                break;
-        }
-        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//
+//        FragmentTransaction ft = fragmentManager.beginTransaction();
+//        ft.add(R.id.container, fragment);
+//        ft.commit();
+        switch(id) {
+            case R.id.nav_first_fragment:
+//                fragment = new LoginActivity();
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                break;
+            case R.id.nav_second_fragment:
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                break;
+            case R.id.nav_third_fragment:
+                startActivity(new Intent(MainActivity.this, PreferenceActivity.class));
+                break;
+            default:
+//                fragment = new LoginActivity();
+                break;
+        }
+//        ft.replace(R.id.container, fragment);
+//
+//        ft.commit();
+
         return true;
     }
 
--- app/src/main/java/com/example/android/bustracker/PreferenceActivity.java
+++ app/src/main/java/com/example/android/bustracker/PreferenceActivity.java
@@ -1,20 +1,16 @@
 package com.example.android.bustracker;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by yishuyan on 5/8/17.
  */
 
-public class PreferenceActivity extends Fragment {
+public class PreferenceActivity extends AppCompatActivity {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-// Inflate the layout for this fragment
-        return inflater.inflate(R.layout.preference, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.preference);
     }
 }
